ea6de06fba51a7e7e0fc4b52bd18847f33b8765b
levanphat1993 <levanphatit1993@gmail.com>
2022-04-28 13:31:12 +0700

    The basic file structure

--- wp-content/themes/nfarm/footer.php
+++ wp-content/themes/nfarm/footer.php
@@ -1,1 +1,13 @@
+<?php 
+	
+	/*
+		This is the template for the footer
+		
+		@package nfarmtheme
+	*/
+	
+?>
+
 <?php wp_footer(); ?>
+</body>
+</html>
--- wp-content/themes/nfarm/inc/enqueue.php
+++ wp-content/themes/nfarm/inc/enqueue.php
@@ -38,14 +38,14 @@ function nfarm_load_admin_scripts($hook)
         wp_register_style('nfarm_admin', get_template_directory_uri() . '/css/nfarm_admin.css', array(), '1.0.0', 'all');
         wp_enqueue_style('nfarm_admin');
         wp_enqueue_media();
-        wp_register_script( 'sunset-admin-script', get_template_directory_uri() . '/js/nfarm_admin.js', array('jquery'), '1.0.0', true );
+        wp_register_script( 'sunset-admin-script', get_template_directory_uri() . '/js/nfarm_admin.js', array('jquery'), '1.0.0', true);
         wp_enqueue_script( 'sunset-admin-script' );
 
     }else if ( 'nfarm_page_alecaddd_nfarm_css' == $hook ){
 		
-		wp_enqueue_style( 'ace', get_template_directory_uri() . '/css/nfarm.ace.css', array(), '1.0.0', 'all' );
-		wp_enqueue_script( 'ace', get_template_directory_uri() . '/js/ace/ace.js', array('jquery'), '1.2.1', true );
-		wp_enqueue_script( 'nfarm-custom-css-script', get_template_directory_uri() . '/js/nfarm.custom_css.js', array('jquery'), '1.0.0', true );
+		wp_enqueue_style( 'ace', get_template_directory_uri() . '/css/nfarm.ace.css', array(), '1.0.0', 'all');
+		wp_enqueue_script( 'ace', get_template_directory_uri() . '/js/ace/ace.js', array('jquery'), '1.2.1', true);
+		wp_enqueue_script( 'nfarm-custom-css-script', get_template_directory_uri() . '/js/nfarm.custom_css.js', array('jquery'), '1.0.0', true);
 	
 	} else { 
         return; 
@@ -55,3 +55,23 @@ function nfarm_load_admin_scripts($hook)
 }
 
 add_action('admin_enqueue_scripts', 'nfarm_load_admin_scripts');
+
+/*
+	
+	========================
+		FRONT-END ENQUEUE FUNCTIONS
+	========================
+*/
+
+function nfarm_load_scripts(){
+	
+	wp_enqueue_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), '3.3.6', 'all');
+	wp_enqueue_style('nfarm', get_template_directory_uri() . '/css/nfarm.css', array(), '1.0.0', 'all');
+	
+	wp_deregister_script('jquery');
+	wp_register_script('jquery', get_template_directory_uri() . '/js/jquery.js', false, '1.11.3', true);
+	wp_enqueue_script('jquery');
+	wp_enqueue_script('bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array('jquery'), '3.3.6', true);
+	
+}
+add_action('wp_enqueue_scripts', 'nfarm_load_scripts');
