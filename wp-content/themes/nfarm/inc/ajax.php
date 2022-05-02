d98c4ce8561875068936c16da96ae152c5f20054
levanphat1993 <levanphatit1993@gmail.com>
2022-05-02 15:17:38 +0700

    build contact page

--- wp-content/themes/nfarm/inc/ajax.php
+++ wp-content/themes/nfarm/inc/ajax.php
@@ -11,6 +11,9 @@
 add_action( 'wp_ajax_nopriv_nfarm_load_more', 'nfarm_load_more' );
 add_action( 'wp_ajax_nfarm_load_more', 'nfarm_load_more' );
 
+add_action( 'wp_ajax_nopriv_nfarm_save_user_contact_form', 'nfarm_save_contact' );
+add_action( 'wp_ajax_nfarm_save_user_contact_form', 'nfarm_save_contact' );
+
 function nfarm_load_more()
 {
 	$paged = $_POST["page"]+1;
@@ -112,4 +115,42 @@ function nfarm_check_paged( $num = null )
 		return $output;
 	}
 	
+}
+
+
+function nfarm_save_contact()
+{
+
+	$title = wp_strip_all_tags($_POST['name']);
+    $email = wp_strip_all_tags($_POST['email']);
+    $message = wp_strip_all_tags($_POST['message']);
+
+    $args = array(
+        'post_title' => $title,
+        'post_content' => $message,
+        'post_author' => 1,
+        'post_status' => 'publish',
+        'post_type' => 'nfarm-contact',
+        'meta_input' => array(
+            '_contact_email_value_key' => $email,
+        ),
+    );
+
+	$postID = wp_insert_post($args);
+
+    if ($postID !== 0) {
+        $to = get_bloginfo('admin_email');
+        $subject = 'Nfarm Contact Form - '.$title;
+
+        $headers[] = 'From: '.get_bloginfo('name').' <'.$to.'>'; // 'From: Dev <me@devmaster.com>'
+        $headers[] = 'Reply-To: '.$title.' <'.$email.'>';
+        $headers[] = 'Content-Type: text/html: charset=UTF-8';
+
+        wp_mail($to, $subject, $message, $headers);
+    }
+
+    echo $postID;
+
+    die();
+
 }
