e75aad1e75217364720499e89497d3ea837ed436
levanphat1993 <levanphatit1993@gmail.com>
2022-05-02 20:13:57 +0700

    update code reponsive

--- wp-content/themes/nfarm/sidebar.php
+++ wp-content/themes/nfarm/sidebar.php
@@ -13,6 +13,18 @@ if (!is_active_sidebar('nfarm-sidebar')){
 ?>
 
 <aside id="secondary" class="widget-area" role="complementary">
+
+	<div class="visible-xs">
+		<?php
+			wp_nav_menu(array(
+				'theme_location' => 'primary',
+				'container' => false,
+				'menu_class' => 'nav navbar-nav navbar-collapse',
+				'walker' => new Nfarm_Walker_Nav_Primary()
+			));	
+		?>
+	</div>
+
 	
 	<?php dynamic_sidebar( 'nfarm-sidebar' ); ?>
 	
