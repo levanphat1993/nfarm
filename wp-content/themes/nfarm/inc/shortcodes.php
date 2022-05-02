7028d8147221f5572ec8da6980522e06bc4edff9
levanphat1993 <levanphatit1993@gmail.com>
2022-05-02 14:44:07 +0700

    contact page

--- wp-content/themes/nfarm/inc/shortcodes.php
+++ wp-content/themes/nfarm/inc/shortcodes.php
@@ -56,3 +56,26 @@ function nfarm_popover( $atts, $content = null )
 }
 
 add_shortcode('popover', 'nfarm_popover');
+
+
+// Contact Form shortcode
+function nfarm_contact_form($atts, $content = null) 
+{
+	
+	//[contact_form]
+	
+	//get the attributes
+	$atts = shortcode_atts(
+		array(),
+		$atts,
+		'contact_form'
+	);
+	
+	//return HTML
+	ob_start();
+	include 'templates/contact-form.php';
+	return ob_get_clean();
+	
+}
+add_shortcode( 'contact_form', 'nfarm_contact_form' );
+
